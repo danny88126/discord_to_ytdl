16e3674309e286c904ecdc9f80265503e7da1b84
cyanwing <dany88126@gmail.com>
2022-06-04 14:18:12 +0800

    補上註解

--- core/classes.py
+++ core/classes.py
@@ -1,6 +1,12 @@
-import discord
-from discord.ext import commands
+# -*- coding: utf-8 -*-
+#Programmer: 易弘翔
+#Date: 2022/06/04
+#定義機器人的文件
 
-class Cog_Extension(commands.Cog):
+
+import discord                       #導入discord套件
+from discord.ext import commands     #導入discord套件中的commands模組
+
+class Cog_Extension(commands.Cog):  #定義機器人本身
     def __init__(self, bot):
         self.bot = bot

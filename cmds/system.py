e71a2fcb763ce479adae155e109489b9c83a764a
cyanwing <dany88126@gmail.com>
2022-06-04 10:05:58 +0800

    調整排版

--- cmds/system.py
+++ cmds/system.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 #Programmer: 易弘翔
-#Date: 2022/06/03
-#機器人COG系統主程式
-#pip install discord.py
-#pip install youtube-dl
+#Date: 2022/06/04
+#機器人系統類別指令
  
+
 import discord                          #導入discord套件
 from discord.ext import commands        #導入discord套件中的commands模組
 from core.classes import Cog_Extension  #導入core資料夾中的Cog_Extension指令
-
-class System(Cog_Extension):            # 定義System類別
+class System(Cog_Extension):            #定義System類別
 
     @commands.command()                                          #設定discord的指令模組
     async def ping(self, ctx):                                   #定義ping指令
